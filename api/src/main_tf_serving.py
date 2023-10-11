5f74ac179f48b16b3f69a62a6aba324c5e18faec
Gavin Xu <guangyuxu@gmail.com>
2023-10-10 22:55:35 -0700

    CORS

--- api/src/main_tf_serving.py
+++ api/src/main_tf_serving.py
@@ -7,10 +7,22 @@ import requests
 import uvicorn
 from PIL import Image
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 
 from parameters import init_parameters
 
 app = FastAPI()
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    # You might want to replace "*" with the specific origins you want to allow
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify specific HTTP methods (GET, POST, etc.) here
+    allow_headers=["*"],  # You can specify specific HTTP headers here
+)
+
 
 (port, endpoint) = init_parameters()
 
@@ -40,9 +52,15 @@ def read_file_as_image(data) -> np.ndarray:
     return np.array(rgb)
 
 
-@app.post("/v1/predict")
+@app.get("/dog-breed/v1/breeds")
+async def breeds():
+    return CLASS_NAMES
+
+
+@app.post("/dog-breed/v1/predict")
 async def predict(file: UploadFile = File(...)):
     image = read_file_as_image(await file.read())
+    print(image)
     image_batch = np.expand_dims(image, 0)
 
     json_data = {"instances": image_batch.tolist()}
